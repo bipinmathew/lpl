48818852105840b1d5b02cd9c434453ef2a82203
Bipin Mathew <bipinmathew@gmail.com>
2015-06-16 23:26:59 -0400

    Added basic support for doubles but had to remove eval logic. Now time to work on getting both going

--- lpl_parser.y
+++ lpl_parser.y
@@ -16,15 +16,35 @@
 %type expr {Node*}
 %extra_argument {bool* valid}
 
-start ::= expr(A) . 
-      {
-        printf("result: %d\n",A->eval());
-      }
+start ::= expr . 
+
 expr(C)  ::= NUMBER(A) PLUS NUMBER(B) . 
     {
-        C = new Node("+",new TerminalNode(atoi(A)),new TerminalNode(atoi(B)));
+        C = new Node("+",new TerminalNode <int> (atoi(A)),new TerminalNode<int>(atoi(B)));
     }
+
+expr(C)  ::= FLOAT(A) PLUS FLOAT(B) . 
+    {
+        C = new Node("+",new TerminalNode <double> (atof(A)),new TerminalNode<double>(atof(B)));
+    }
+
+expr(C)  ::= FLOAT(A) PLUS NUMBER(B) . 
+    {
+        C = new Node("+",new TerminalNode <double> (atof(A)),new TerminalNode<int>(atof(B)));
+    }
+
+
+expr(C)  ::= NUMBER(A) PLUS FLOAT(B) . 
+    {
+        C = new Node("+",new TerminalNode <int> (atof(A)),new TerminalNode<double>(atof(B)));
+    }
+
 expr(C) ::= expr(A) PLUS NUMBER(B) .
     {
-        C = new Node("+",A,new TerminalNode(atoi(B)));
+        C = new Node("+",A,new TerminalNode<int>(atoi(B)));
+    }
+
+expr(C) ::= expr(A) PLUS FLOAT(B) .
+    {
+        C = new Node("+",A,new TerminalNode<double>(atof(B)));
     }
--- node.h
+++ node.h
@@ -1,11 +1,14 @@
 #ifndef NODE_H
 #define NODE_H
 #include <string>
+#include <iostream>
+  template <class T> class Result {};
+
   class Node {
     public:
-        virtual int eval();
-        int evalLeft();
-        int evalRight();
+        // virtual Result <class T> * eval();
+        // Result <class> * evalLeft();
+        // Result <class> evalRight();
         void setLeft(Node* left);
         void setRight(Node* right);
         Node(const std::string& input, Node *l,Node *r);
@@ -15,12 +18,32 @@
         Node *l, *r;
   };
 
+  template <class T>
   class TerminalNode : public Node {
       public:
-          virtual int eval();
-          void setValue(int value);
-          TerminalNode(int value);
+          virtual T eval();
+          void setValue(T value);
+          TerminalNode(T value);
       private:
-          int v;
+          T v;
   };
+
+
+    template <class T>
+    TerminalNode<T>::TerminalNode(T value){
+        std::cout << value << std::endl;
+        setValue(value);
+    }
+
+
+    template <class T>
+    void TerminalNode<T>::setValue(T value){
+        v=value;
+    }
+
+
+    template <class T>
+    T TerminalNode<T>::eval(){
+        return(v);
+    }
 #endif
