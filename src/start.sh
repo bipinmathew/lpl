36dd713820a1f749478ab920f4abb580a245b921
Bipin Mathew <bipinmathew@gmail.com>
2016-01-18 21:06:16 -0500

    Removing submodule tip since we will not be using it directly yet. Instead we will be bringing libcol.

---
+++ src/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gvim lpl.c parser/nodes/node.c parser/nodes/node.h tip/col.c tip/col.h &
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gvim src/lpl.c src/parser/nodes/node.c src/parser/nodes/node.h
