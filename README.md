98c0e3cb7d28f28443122e5b3a16d573273127c0
Bipin Mathew <bipinmathew@gmail.com>
2016-02-08 22:47:33 -0500

    Improved timing functions.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LPL Programming Language
 LPL is a programming language designed for vector workflows. It strives to be as versatile as matlab and and as terse as APL. 
+
+## Nouns
+Currently the following nouns are available.
+
+1. **Integers**
+2. **Floats**
+3. **Integer arrays**
+4. **Double arrays**
+
+## Verbs
+
+The following verbs are available.
+
+1. The normal dyadic calculator functions for integers and floats. Integer operations will be promoted to double if needed.
+2. +/ Sum over
+3. ! Bang
+
--- src/lpl.c
+++ src/lpl.c
@@ -9,24 +9,38 @@ int main() {
     char commandLine[1024];
     node *result;
     clock_t start, diff;
+    time_t wstart;
+    double wtime;
     int msec;
+
+
+    struct timespec now, tmstart;
 
 
     while (fgets(commandLine,1024,stdin)) {
 #ifdef DEBUG
-      fp = fopen("errors.log","w");
-      ParseTrace(fp,"err: ");
+      /* fp = fopen("errors.log","w");
+      ParseTrace(fp,"err: "); */
 #endif
-      start = clock();
-      result=parse(commandLine);
-      diff = clock()-start;
+
+      clock_gettime(CLOCK_REALTIME, &tmstart);
+
+        start = clock();
+          result=parse(commandLine);
+          printNode(result);
+          printf("\n");
+        diff = clock()-start;
+
+      clock_gettime(CLOCK_REALTIME, &now);
+
+      wtime = 1000*((double)((now.tv_sec+now.tv_nsec*1e-9) - (double)(tmstart.tv_sec+tmstart.tv_nsec*1e-9)));
+
+
       msec = diff*1000 / CLOCKS_PER_SEC;
 
-      printNode(result);
-      printf("\n");
-      printf("%s, cputime: %d ms\n",commandLine,msec);
+      printf("%s, walltime: %f, cputime: %d ms\n",commandLine,wtime,msec);
 #ifdef DEBUG
-      fclose(fp);
+      /* fclose(fp); */
 #endif
     }
 
